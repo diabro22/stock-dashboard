3707e894ca9c8435eaf664327530396b3fed11c3
diabro22 <devinroberts2001@gmail.com>
2023-07-03 16:49:32 -0400

    display coming soon message as placeholder for dashboard

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const { restClient } = require('@polygon.io/client-js');
-const rest = restClient("7I1Tz8oO2vTT4pVlNf3O_N9mC0IOKIme");
-rest.stocks.lastTrade("AAPL").then((data) => {
-    console.log(data);
-}).catch(e => {
-    console.error('An error happened: ', e);
-});
+const requirejs = require('./scripts/require.js');
+const restClient = require('./node_modules/@polygon.io/client-js/dist/main');
+const rest = restClient ("7I1Tz8oO2vTT4pVlNf3O_N9mC0IOKIme");
 
 rest.stocks.lastQuote("AAPL").then((data) => {
     console.log(data);
